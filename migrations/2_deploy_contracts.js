86ca3bc207480c9cffe82ce8247cf96894c1707e
Tristan | Tribot17 <tribot17@gmail.com>
2022-03-22 14:51:54 +0100

    commit 03/22

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,7 @@
 var Rec = artifacts.require("./Rec.sol");
-// var StackingCore = artifacts.require("./StakingCore.sol");
 var Ower = artifacts.require("./Ower.sol");
 
 module.exports = async function (deployer, _network, accounts) {
   await deployer.deploy(Rec);
-  // await deployer.deploy(Token);
   await deployer.deploy(Ower);
-  // const rec = await Rec.deployed();
-  // await deployer.deploy(StackingCore, rec.address);
 };
